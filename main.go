0f1e7ee73f5081018276fb9a6b974c4ed5be1580
zhangzhiming <zhangzhiming>
2023-03-28 10:22:54 +0800

    print resp

--- main.go
+++ main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := "https://www.thepaper.cn"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("fetch url error:%v", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error statu code:%v", resp.StatusCode)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("read err %v", err)
+		return
+	}
+
+	fmt.Println("body:", string(body))
+}
