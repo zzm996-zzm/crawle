c1add3cecc9709a8417bb8918d9c93eceb243d14
zhangzhiming <zhangzhiming>
2023-03-28 10:46:02 +0800

    up

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module crawler
 
 go 1.18
+
+require (
+	golang.org/x/net v0.8.0
+	golang.org/x/text v0.8.0
+)
--- main.go
+++ main.go
@@ -1,31 +1,57 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
 func main() {
-	url := "https://www.thepaper.cn"
-	resp, err := http.Get(url)
+	url := "https://www.chinanews.com.cn/"
+	body, err := Fetch(url)
+
 	if err != nil {
-		fmt.Println("fetch url error:%v", err)
+		fmt.Printf("read content failed:%v", err)
 		return
+	}
+
+	fmt.Println(string(body))
+}
+
+func Fetch(url string) ([]byte, error) {
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error statu code:%v", resp.StatusCode)
-		return
+		fmt.Printf("Error status code:%d", resp.StatusCode)
 	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
 
-	body, err := ioutil.ReadAll(resp.Body)
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+
+	bytes, err := r.Peek(1024)
+
 	if err != nil {
-		fmt.Printf("read err %v", err)
-		return
+		fmt.Printf("fetch error:%v", err)
+		return unicode.UTF8
 	}
 
-	fmt.Println("body:", string(body))
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
